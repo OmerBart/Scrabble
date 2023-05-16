6284095fe7030c2f57a1da9c9a6d3882da9c4bc1
OmerBart <omer@cs.colman.ac.il>
2023-05-16 15:52:59 +0300

    Added model files

--- src/main/java/com/example/Scrabble/Model/Model.java
+++ src/main/java/com/example/Scrabble/Model/Model.java
@@ -1,4 +1,9 @@
 package com.example.Scrabble.Model;
 
 public interface Model {
+    public String getName();
+    public void setName(String name);
+    public void setID();
+    //add client handler methods to do with the game here
+
 }
