f705f12fc50937e9a5fa5d1b9c32824a47a03128
eilon <eilon159123@gmail.com>
2023-05-16 22:32:25 +0300

    p

--- src/main/java/com/example/Scrabble/Test/TestHostModel.java
+++ src/main/java/com/example/Scrabble/Test/TestHostModel.java
@@ -13,9 +13,7 @@ public class TestHostModel {
         // Random r=new Random();
         // int port=6000+r.nextInt(1000);
         h.connectServer();
-        h.BookActions("Q,s1.txt,s2.txt,19393");
-
-
+        
 
     }
 
