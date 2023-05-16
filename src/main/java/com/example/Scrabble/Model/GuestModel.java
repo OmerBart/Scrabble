2d767a4f635fe3f9a229d0c29e9de71d6291f870
OmerBart <omer@cs.colman.ac.il>
2023-05-16 15:54:02 +0300

    Added model files

---
+++ src/main/java/com/example/Scrabble/Model/GuestModel.java
@@ -0,0 +1,20 @@
+package com.example.Scrabble.Model;
+
+public class GuestModel implements Model{
+    private String name;
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public void setID() {
+
+    }
+}
