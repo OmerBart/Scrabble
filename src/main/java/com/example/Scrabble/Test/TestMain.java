2ee4dd228fceb2a211640be0414b76852a3fa117
OmerBart <omer@cs.colman.ac.il>
2023-05-21 19:27:35 +0300

    Last commit before major change

--- src/main/java/com/example/Scrabble/Test/TestMain.java
+++ src/main/java/com/example/Scrabble/Test/TestMain.java
@@ -10,18 +10,18 @@ import com.example.Scrabble.Model.HostPlayer;
 public class TestMain {
 
     public static void main(String[] args) {
-        HostModel h = new HostModel("Hadar");
-        System.out.println("Host created");
+//        HostModel h = new HostModel("Hadar");
+//        System.out.println("Host created");
         // Random r=new Random();
         // int port=6000+r.nextInt(1000);
-        h.connectServer();
-        System.out.println("Server connected");
-        h.test("waka");
-        h.dictionaryRequest("Q,s1.txt,s2.txt,19393");
-        GuestPlayer gp = new GuestPlayer();
-        System.out.println("Guest created " + gp);
-        HostPlayer hp = new HostPlayer();
-        System.out.println("Host created " + hp.getName());
+//        h.connectServer();
+//        System.out.println("Server connected");
+//        h.test("waka");
+//        h.dictionaryRequest("Q,s1.txt,s2.txt,19393");
+//        GuestPlayer gp = new GuestPlayer();
+//        System.out.println("Guest created " + gp);
+//        HostPlayer hp = new HostPlayer();
+//        System.out.println("Host created " + hp.getName());
 
 
 
