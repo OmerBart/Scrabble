89095c1accbf7aad7ca566c99b704bba7a03d1b7
eilon <eilon159123@gmail.com>
2023-05-24 16:32:56 +0300

    guestplayer test

---
+++ src/test/java/com/example/Scrabble/Model/GuestPlayerTest.java
@@ -0,0 +1,56 @@
+package com.example.Scrabble.Model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GuestPlayerTest {
+    GuestPlayer guestPlayer;
+
+    @BeforeEach
+    void setUp() {
+        guestPlayer = new GuestPlayer("test", 1, "localhost:8080");
+        System.out.println("GuestPlayer: setUp()");
+    }
+
+    @AfterEach
+    void tearDown() {
+        guestPlayer = null;
+        System.out.println("GuestPlayer: tearDown()");
+    }
+
+    @Test
+    void getPlayerID() {
+        assertEquals(1, guestPlayer.getPlayerID());
+    }
+
+    @Test
+    void getName() {
+        assertEquals("test", guestPlayer.getName());
+    }
+
+    @Test
+    void getServerAddress() {
+        assertEquals("localhost:", guestPlayer.getServerAddress().substring(0, 9));
+    }
+
+    @Test
+    void setServerAddress() {
+        guestPlayer.setServerAddress("localhost", 8080);
+        assertEquals("localhost:8080", guestPlayer.getServerAddress());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("GuestPlayer|test|1|localhost:8080|", guestPlayer.toString());
+    }
+
+    @Test
+    void testEquals() {
+        GuestPlayer guestPlayer1 = new GuestPlayer("test", 1, "localhost:8080");
+        assertEquals(true, guestPlayer.equals(guestPlayer1));
+    }
+
+}
