c768a92b90d1873a59f0aed691f50d41468e5235
eilon <eilon159123@gmail.com>
2023-05-24 16:19:23 +0300

    hostTest

--- src/test/java/com/example/Scrabble/Model/HostPlayerTest.java
+++ src/test/java/com/example/Scrabble/Model/HostPlayerTest.java
@@ -8,23 +8,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HostPlayerTest {
 
+    HostPlayer hostPlayer;
+
     @BeforeEach
     void setUp() {
+        hostPlayer = new HostPlayer(new GuestPlayer("test", 1, "localhost:8080"));
+        System.out.println("HostPlayerTest: setUp()");
     }
 
     @AfterEach
     void tearDown() {
+        hostPlayer = null;
+        System.out.println("HostPlayerTest: tearDown()");
     }
 
     @Test
     void getHostgameServer() {
+        assertNotNull(hostPlayer.getHostgameServer());
     }
 
     @Test
     void stopGame() {
+        hostPlayer.stopGame();
+        assertEquals(true, hostPlayer.getHostgameServer().isStop());
     }
 
     @Test
     void testToString() {
+        assertEquals("HostPlayer|test|1|localhost:", hostPlayer.toString().substring(0, 28));
     }
 }
