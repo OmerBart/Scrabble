bafb4108eb7e792c2321ec99751dafef94ee6fc8
eilon <eilon159123@gmail.com>
2023-05-26 22:23:16 +0300

    clean code

--- src/test/java/com/example/Scrabble/Game/GameManagerTest.java
+++ src/test/java/com/example/Scrabble/Game/GameManagerTest.java
@@ -8,8 +8,8 @@ import com.example.Scrabble.Model.Game.GameManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeEach;
 
 public class GameManagerTest {
 
--- src/test/java/com/example/Scrabble/Model/HostPlayerTest.java
+++ src/test/java/com/example/Scrabble/Model/HostPlayerTest.java
@@ -1,7 +1,7 @@
 package com.example.Scrabble.Model;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
