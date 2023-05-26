d80a2a70f20276bd7b30a19ff8f9029d47519495
OmerBart <omer@cs.colman.ac.il>
2023-05-26 14:59:01 +0300

    fixing bad merge

--- src/main/java/com/example/Scrabble/Model/HostPlayer.java
+++ src/main/java/com/example/Scrabble/Model/HostPlayer.java
@@ -1,6 +1,7 @@
 package com.example.Scrabble.Model;
 
-import com.example.Scrabble.Game.GameManager;
+
+import com.example.Scrabble.Model.Game.GameManager;
 import com.example.Scrabble.ScrabbleServer.MyServer;
 
 import java.util.Random;
