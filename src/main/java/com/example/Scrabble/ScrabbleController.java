3eb7f3d601ae0504a4a8d59e2fc51c07313c9dfe
eilon <eilon159123@gmail.com>
2023-06-07 15:28:40 +0300

    start canvas

--- src/main/java/com/example/Scrabble/ScrabbleController.java
+++ src/main/java/com/example/Scrabble/ScrabbleController.java
@@ -1,13 +1,15 @@
 package com.example.Scrabble;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
-
-// import com.example.Scrabble.BoardCanvas;
 
 public class ScrabbleController {
     @FXML
     private Label welcomeText;
+
+    @FXML   
+    private Canvas canvas;
 
     @FXML
     protected void onJoinGameButtonClick() {
@@ -18,4 +20,6 @@ public class ScrabbleController {
     protected void onHostGameButtonClick() {
         welcomeText.setText("Welcome to Scrabble! (Host)");
     }
+
+    
 }
