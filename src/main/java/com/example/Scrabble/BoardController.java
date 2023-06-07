0719cc683d994910eb602c00d847a80bad9c5dab
eilon <eilon159123@gmail.com>
2023-06-07 19:42:29 +0300

    board

--- src/main/java/com/example/Scrabble/BoardController.java
+++ src/main/java/com/example/Scrabble/BoardController.java
@@ -2,6 +2,7 @@ package com.example.Scrabble;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
@@ -17,16 +18,39 @@ public class BoardController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         boardBuild();
+        boardBordersBuild();
     }
 
     public void boardBuild() {
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
                 Rectangle rect = new Rectangle(40, 40);
-                rect.getStyleClass().add("board-tile");
+                rect.styleProperty().set(
+                        "-fx-fill: 'green'; -fx-stroke: #000000; -fx-stroke-width: 1; -fx-arc-width: 5; -fx-arc-height: 5; -fx-alignment: center;");
                 board.add(rect, i, j);
             }
         }
     }
 
+    public void boardBordersBuild() {
+        board.getChildren().get(0).styleProperty().set("-fx-display: 'none';");
+        for (int i = 1; i < 16; i++) {
+            String letter = String.valueOf((char) (i + 64));
+            Label label = new Label(letter);
+            label.styleProperty()
+                    .set("-fx-font-size: 20px; -fx-text-fill: 'black'; -fx-margin-left:5px;");
+            board.add(label, i, 0);
+            board.getChildren().get(i).styleProperty().set(
+                    "-fx-fill: 'white'; -fx-stroke: #000000; -fx-stroke-width: 1; -fx-arc-width: 5; -fx-arc-height: 5;");
+        }
+        for (int i = 1; i < 16; i++) {
+            Label label = new Label(String.valueOf(i));
+            label.setRotate(270);
+            label.styleProperty()
+                    .set("-fx-font-size: 20px; -fx-text-fill: 'black'; -fx-margin-left:105px;");
+            board.add(label, 0, i);
+            board.getChildren().get(i * 16).styleProperty().set(
+                    "-fx-fill: 'white'; -fx-stroke: #000000; -fx-stroke-width: 1; -fx-arc-width: 5; -fx-arc-height: 5;");
+        }
+    }
 }
