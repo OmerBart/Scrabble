7599e5f3e9f941aefcabd1e61ee21ca3bd8fd93f
eilon <eilon159123@gmail.com>
2023-06-08 13:48:31 +0300

    board done

--- src/main/java/com/example/Scrabble/BoardController.java
+++ src/main/java/com/example/Scrabble/BoardController.java
@@ -7,10 +7,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Stack;
 
 public class BoardController implements Initializable {
 
@@ -30,15 +30,12 @@ public class BoardController implements Initializable {
     public void boardBuild() {
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 16; j++) {
-                Rectangle rect = new Rectangle(40, 40);
-                StackPane stack = new StackPane(rect, new Label(""));
-                stack.setAlignment(Pos.CENTER);
-                rect.getStyleClass().add("board-cell");
-                board.add(rect, i, j);
+                newCellBuilder(i, j);
             }
         }
         boardBordersBuild();
         drawStar();
+        drawBonus();
     }
 
     public void boardBordersBuild() {
@@ -74,5 +71,122 @@ public class BoardController implements Initializable {
         StackPane stack = new StackPane(rect, label);
         stack.setAlignment(Pos.CENTER);
         board.add(stack, 8, 8);
+    }
+
+    public void drawBonus() {
+        newCellBuilder("3W", 1, 1);
+        newCellBuilder("3W", 1, 8);
+        newCellBuilder("3W", 1, 15);
+        newCellBuilder("3W", 8, 1);
+        newCellBuilder("3W", 8, 15);
+        newCellBuilder("3W", 15, 1);
+        newCellBuilder("3W", 15, 8);
+        newCellBuilder("3W", 15, 15);
+
+        newCellBuilder("2W", 2, 2);
+        newCellBuilder("2W", 3, 3);
+        newCellBuilder("2W", 4, 4);
+        newCellBuilder("2W", 5, 5);
+        newCellBuilder("2W", 11, 11);
+        newCellBuilder("2W", 12, 12);
+        newCellBuilder("2W", 13, 13);
+        newCellBuilder("2W", 14, 14);
+        newCellBuilder("2W", 2, 14);
+        newCellBuilder("2W", 3, 13);
+        newCellBuilder("2W", 4, 12);
+        newCellBuilder("2W", 5, 11);
+        newCellBuilder("2W", 11, 5);
+        newCellBuilder("2W", 12, 4);
+        newCellBuilder("2W", 13, 3);
+        newCellBuilder("2W", 14, 2);
+
+        newCellBuilder("2L", 1, 4);
+        newCellBuilder("2L", 4, 1);
+        newCellBuilder("2L", 1, 12);
+        newCellBuilder("2L", 12, 1);
+        newCellBuilder("2L", 3, 7);
+        newCellBuilder("2L", 7, 3);
+        newCellBuilder("2L", 4, 8);
+        newCellBuilder("2L", 8, 4);
+        newCellBuilder("2L", 9, 3);
+        newCellBuilder("2L", 3, 9);
+        newCellBuilder("2L", 15, 4);
+        newCellBuilder("2L", 4, 15);
+        newCellBuilder("2L", 7, 7);
+        newCellBuilder("2L", 7, 9);
+        newCellBuilder("2L", 9, 7);
+        newCellBuilder("2L", 9, 9);
+        newCellBuilder("2L", 12, 8);
+        newCellBuilder("2L", 8, 12);
+        newCellBuilder("2L", 7, 13);
+        newCellBuilder("2L", 13, 7);
+        newCellBuilder("2L", 9, 13);
+        newCellBuilder("2L", 13, 9);
+        newCellBuilder("2L", 4, 15);
+        newCellBuilder("2L", 15, 4);
+        newCellBuilder("2L", 12, 15);
+        newCellBuilder("2L", 15, 12);
+
+        newCellBuilder("3L", 2, 6);
+        newCellBuilder("3L", 6, 2);
+        newCellBuilder("3L", 2, 10);
+        newCellBuilder("3L", 10, 2);
+        newCellBuilder("3L", 6, 6);
+        newCellBuilder("3L", 6, 10);
+        newCellBuilder("3L", 10, 6);
+        newCellBuilder("3L", 10, 10);
+        newCellBuilder("3L", 6, 14);
+        newCellBuilder("3L", 14, 6);
+        newCellBuilder("3L", 10, 14);
+        newCellBuilder("3L", 14, 10);
+    }
+
+    private void newCellBuilder(int r, int c) {
+        Label label = new Label("");
+        Rectangle rect = new Rectangle(40, 40);
+        if (r == 0 && c == 0) {
+            rect.getStyleClass().add("hide");
+        } else {
+            rect.getStyleClass().add("board-cell");
+        }
+        StackPane stack = new StackPane(rect, label);
+        stack.setAlignment(Pos.CENTER);
+        board.add(stack, c, r);
+    }
+
+    private void newCellBuilder(String bonus, int r, int c) {
+        Label label = new Label("");
+        Rectangle rect = new Rectangle(40, 40);
+        switch (bonus) {
+            case "2L":
+                label = new Label("2L");
+                rect = new Rectangle(40, 40);
+                rect.getStyleClass().add("bonus-2L");
+                break;
+
+            case "3L":
+                label = new Label("3L");
+                rect = new Rectangle(40, 40);
+                rect.getStyleClass().add("bonus-3L");
+                break;
+
+            case "2W":
+                label = new Label("2W");
+                rect = new Rectangle(40, 40);
+                rect.getStyleClass().add("bonus-2W");
+                break;
+
+            case "3W":
+                label = new Label("3W");
+                rect = new Rectangle(40, 40);
+                rect.getStyleClass().add("bonus-3W");
+                break;
+
+            default:
+                break;
+        }
+        StackPane stack = new StackPane(rect, label);
+        stack.setAlignment(Pos.CENTER);
+        board.add(stack, c, r);
     }
 }
